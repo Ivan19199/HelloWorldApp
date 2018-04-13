86e419ec853783a458531114630ae455eda3037b
Hryhorii Klochnyk <klo4nik@gmail.com>
2018-04-13 19:12:04 +0300

    Created base platform for coders names
    You can edit this file to add your name

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+    Created by Hryhorii Klochnyk 13.04.2018
+    Lviv National University "DVVS" project
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string names[] = {"Hryhorii"}; // Write your name here
+    std::cout << "Hello World from:\n";
+    for(int i = 0; i < (sizeof(names)/sizeof(*names)); i++)
+    {
+        std::cout << names[i] << std::endl;
+    }
+    return 0;
+}
